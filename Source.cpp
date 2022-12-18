050cfab61147cf6a5c4062e77c3b58b19a4d4fdb
Anas-Altaf <anashafiz926@gmail.com>
2022-12-18 13:43:34 +0500

    V 1.1
    1 Mode Cleared without Timer

--- Source.cpp
+++ Source.cpp
@@ -1,22 +1,22 @@
+//Candy Crush Game with Colors and Symbols
 #include<iostream>
 #include<windows.h>
 #include<cstdlib>
-#include<stdlib.h>
 #include<ctime>
 #include<string>
-#include<conio.h>
 #include<iomanip>
+#include<fstream>
+#include<cstring>
 #include"headerfile.hpp"
 using namespace std;
 int main()
 {
-	char board[ROW][COL];
 	//Coloring
 	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(h, 1);
 	int action=1;
 	bool exit=true;
-	FirstBoardGenerator(board);
+	FirstBoardGenerator();
 	GameName();
 	SelectOption();
 	while(action!=0&&action<5)
@@ -25,7 +25,7 @@ int main()
 	cin>>action;
 		switch(action)
 		{
-			case 1: PlayGame(board); break;
+			case 1: PlayGame(); break;
 			case 2: Credits(); break;
 			case 3: Instructions(); break;
 			case 4: HighScores(); break;
